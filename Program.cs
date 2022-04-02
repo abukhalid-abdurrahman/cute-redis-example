162a5bd64c5067423d4c7c58b03298f5b4fd572e
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-04-02 18:42:04 +0500

    saving and getting data from redis

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace CuteRedisExample
+﻿using System;
+using StackExchange.Redis;
+
+namespace CuteRedisExample
 {
-    static class Program
+    internal static class Program
     {
-        public static void Main()
+        private static string RedisConnection => "127.0.0.1:6379";
+        public static void Main(string[] args)
         {
+            // Store user to redis cache
+            var userId = Guid.NewGuid().ToString();
+            Set(userId, "Bob");
+            Console.WriteLine("User saved to redis cache");
+            
+            // Get user from redis store
+            var userName = Get(userId);
+            Console.WriteLine($"User loaded from redis cache: {userName}");
+        }
+
+        private static void Set(string key, string value)
+        {
+            using IConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(RedisConnection);
+            var db = connectionMultiplexer.GetDatabase();
+            db.StringSet(key, value);
+        }
         
+        private static string Get(string key)
+        {
+            using IConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(RedisConnection);
+            var db = connectionMultiplexer.GetDatabase();
+            return db.StringGet(key);
         }
     }
 }
